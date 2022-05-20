79bab56a07d10936e9c29d218e5c343afec718df
Zuhriddin Sadriddinov <100588044+iZuhriddin@users.noreply.github.com>
2022-05-20 08:44:05 +0500

    Update delmem.py

--- delmem.py
+++ delmem.py
@@ -1,22 +1,27 @@
-from telethon import TelegramClient
-import os
-Bot = TelegramClient('MemeberRomover',int(input('17299114')),str(input('653741f09cbd2a8839c21cdf5fe46217'))).start(bot_token=str(input('5232848304:AAFyoNZigpThyz-FVMs9f2_NQulsuu9cmtQ')))
-Target = input('@kulgu_vaqti_uz')
-RemoveCount = input('6500')
-async def Main() :
-    MeID = await Bot.get_me().id
-    i = 0
-    while i < int(RemoveCount) :
-        for User in await Bot.iter_participants(entity=Target) :
-            if i >= int(RemoveCount) :
-                break
-            try :
-                if int(MeID) != int(User.id) :
-                    await Bot.kick_participant(Target,int(User.id))
-                    i = i + 1
-            except :
-                pass
-    print(f'[!] Deleted {i} User From {Target} !')
-with Bot :
-    Bot.loop.run_until_complete(Main())
+from telethon import TelegramClient 
+import os 
+bit_tokken= "5232848304:AAFyoNZigpThyz-FVMs9f2_NQulsuu9cmtQ" 
+api_id=17299114
+
+api_hash="653741f09cbd2a8839c21cdf5fe46217" 
+kanal = "@kulgu_vaqti_uz"
+Bot = TelegramClient('MemeberRomover',api_id,api_hash).start(bot_token=bit_tokken) 
+Target = kanal
+RemoveCount = input('6500') 
+async def Main() : 
+    MeID = await Bot.get_me().id 
+    i = 0 
+    while i < int(RemoveCount) : 
+        for User in await Bot.iter_participants(entity=Target) : 
+            if i >= int(RemoveCount) : 
+                break 
+            try : 
+                if int(MeID) != int(User.id) : 
+                    await Bot.kick_participant(Target,int(User.id)) 
+                    i = i + 1 
+            except : 
+                pass 
+    print(f'[!] Deleted {i} User From {Target} !') 
+with Bot : 
+    Bot.loop.run_until_complete(Main()) 
 os.remove('MemeberRomover.session')
